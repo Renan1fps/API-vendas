6e66192a7c02a2278b669f41de9c205fa14ee3d3
Renan Oliveira <renan.sergipe68@gmail.com>
2021-06-11 18:01:49 -0300

    Customizando tratamento de erros

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,12 +1,27 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import { router } from './routes';
+import { AppError } from '@shared/errors/AppError';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(router)
+app.use(router);
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+    return response.status(500).json({
+      status: 'error',
+      message: 'internal server error',
+    });
+  },
+);
 
 app.listen(8080, () => {
   console.log('Server running');
