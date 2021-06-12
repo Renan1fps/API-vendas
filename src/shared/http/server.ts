6eeb73f0e47af12d509d9036bbc79ae4d15080e6
Renan Oliveira <renan.sergipe68@gmail.com>
2021-06-12 17:59:24 -0300

    Criação do Banco de Dados e Configuração da Conexão com o TypeORM

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,6 +3,7 @@ import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import { router } from './routes';
 import { AppError } from '@shared/errors/AppError';
+import '@shared/typeorm';
 
 const app = express();
 
