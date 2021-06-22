88baa1f93c7ef1f39efc927df38eda2cddd58764
Renan Oliveira <renan.nunes@mblabs.com.br>
2021-06-22 10:14:57 -0300

    Serviço de produtos (ShowProductService)

---
+++ src/modules/products/services/ShowProductService.ts
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+import { IIdRequest } from '@shared/contracts/IdRequest';
+import { AppError } from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import { Product } from '../typeorm/Model/Product';
+import { ProductRepository } from '../typeorm/repositories/ProductRepository';
+
+class ShowProductService {
+
+  public async execute({ id }: IIdRequest): Promise<Product | undefined> {
+
+    const productRepository = getCustomRepository(ProductRepository);
+
+    const product = await productRepository.findOne(id);
+
+    if(!product){
+        throw new AppError('Product not found')
+    }
+
+    return product;
+
+  }
+
+}
+
+export { ShowProductService }
---
+++ src/shared/contracts/IdRequest.ts
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+export interface IIdRequest {
+
+  id: string;
+
+}
