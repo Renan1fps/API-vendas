b47e77513c11a02666c0e4c53bda9359ad8dd51d
Renan Oliveira <renan.sergipe68@gmail.com>
2021-06-27 00:54:28 -0300

    Serviço de usuarios (UpdateUserService)

---
+++ src/modules/users/services/UpdateUserService.ts
@@ -0,0 +1,24 @@
+import { IHttUserRequest } from '@shared/contracts/IHttpUserRequest';
+import { IIdRequest } from '@shared/contracts/IIdRequest';
+import { AppError } from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import { User } from '../typeorm/entities/User';
+import { UserRepository } from '../typeorm/repositories/UserRepository';
+
+class UpdateUserService {
+  public async execute(
+    { id }: IIdRequest,
+    { email, name, password }: IHttUserRequest,
+  ): Promise<User | undefined> {
+    const updateUser = getCustomRepository(UserRepository);
+
+    const findUser = await updateUser.findById(id);
+    if (!findUser) {
+      throw new AppError('User not found');
+    }
+    const user = updateUser.create({ email, name, password });
+    await updateUser.save(user);
+    return user;
+  }
+}
+export { UpdateUserService }
