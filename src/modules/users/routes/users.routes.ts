f6caf9f00c35b0fdaebe99355be3f6a57ea01f21
Renan Oliveira <renan.nunes@mblabs.com.br>
2021-07-01 21:31:49 -0300

    Adicionando validaçao na rota user

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,11 +1,19 @@
 /* eslint-disable prettier/prettier */
 import { Router } from "express";
 import { UserController } from "../controllers/UserController";
+import { celebrate, Joi, Segments } from "celebrate";
 
 const userRoutes = Router()
 const userController = new UserController()
 
-userRoutes.get('/', userController.index)
-userRoutes.post('/', userController.create)
+userRoutes.get('/',userController.index)
+
+userRoutes.post('/', celebrate({
+    [Segments.BODY]:{ 
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required()
+  }
+}), userController.create)
 
 export { userRoutes }
